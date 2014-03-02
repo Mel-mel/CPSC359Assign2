2715c76996dacce298c7c78bcb9d66432717c33d
emCsq <emcsq@live.ca>
2014-03-02 13:57:35 -0700

    Update main.s
    teehee

--- main.s
+++ main.s
@@ -18,24 +18,23 @@ loopChar:
 
     add r0, r0, #0x1   
     cmp r0, #0x5A
-    blt loopChar
+    ble loopChar
     
 haltLoop$:
     b haltLoop$
    
 //****************************************************** 
 putChar:
-    ldr r3, =UART_AUX_MU_IO_REG
+    ldr r3, =UART_AUX_MU_LSR_REG
 
 inLoop:
-    //Need to wait until bit 5 can accept at least one byte
+    //Need to wait until bit 5 is set
     ldr r1, [r3]
     tst r1, #0x20 						//Testing bit 5
     beq inLoop
 
     //Write the character to the IO register
     ldr r2, =UART_AUX_MU_IO_REG
-    //ldr r3, 0x41						// write current ASCII
     str r3, [r2]
  
     mov pc, lr
@@ -46,10 +45,12 @@ getChar:
     ldr r0, =UART_AUX_MU_LSR_REG
     
 getCharLoop:
+    //wait until data is ready (LSR bit 0 is set) 
     ldr r1, [r2]
     tst r1, #0x1 						//We have to test bit 0
     beq getCharLoop
-    
+       
+    //write character to IO register
     ldr r2, =UART_AUX_MU_IO_REG
     ldr r0, [r2]
     
